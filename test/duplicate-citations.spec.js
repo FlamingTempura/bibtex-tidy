0e0f51900e14c1706893d346bec7177655ee7476
Peter West <peter.g.west@gmail.com>
2020-05-16 20:44:27 +0100

    Adding unit test for #11 (incorrect flagging of duplicate titles)

--- test/duplicate-citations.spec.js
+++ test/duplicate-citations.spec.js
@@ -8,7 +8,29 @@ const input = bibtex`
 @article{b,
     author={Smith, JA},
     title={Something blah blah}
-}`;
+}
+Issue #11 - these should not be flagged as dupelicates
+@Article{Raku2,
+	title = {Focusing of a vortex carrying beam with Gaussian background by a lens in the presence of spherical aberration and defocusing},
+	author = {R.K.Singh and P.Senthilkumaran and K. Singh},
+	year = 2007,
+	journal = {Optics and Lasers in Engg.,},
+	volume = 45,
+	pages = {773--782},
+	date-added = {2020-05-09 00:08:30 +0530},
+	date-modified = {2020-05-09 00:10:16 +0530}
+	}
+	
+	@Article{Raku10,
+	title = {Focusing of a vortex carrying beam with Gaussian background by an apertured system in presence of coma},
+	author = {R.K.Singh and P.Senthilkumaran and K. Singh},
+	year = 2008,
+	journal = {Opt.Commun.},
+	volume = 281,
+	pages = {923--934},
+	date-added = {2020-05-09 00:13:37 +0530},
+	date-modified = {2020-05-09 00:14:50 +0530}
+	}`;
 
 test(
 	'duplicate citation warnings',
