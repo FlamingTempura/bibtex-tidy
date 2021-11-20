0ea079908f1aa63e6b8e3b01a8bacc15196d2835
Peter West <peter.g.west@gmail.com>
2021-11-20 23:01:26 +0000

    manpage

--- build.ts
+++ build.ts
@@ -130,7 +130,7 @@ async function generateManPage() {
 		banner + `export const manPage = ${JSON.stringify(manPage, null, '\t')};`
 	);
 
-	await writeFile(join('mainpage.txt'), manPage.join('\n'));
+	await writeFile('bibtex-tidy.0', manPage.join('\n'));
 
 	const readme = await readFile(join(__dirname, 'README.md'), 'utf8');
 	await writeFile(
