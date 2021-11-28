be9f4be060d3d82b289ca349aef9ba720338b369
Peter West <peter.g.west@gmail.com>
2021-11-28 10:40:08 +0000

    New release process
    Use `npm version patch` to generate a new release. This:
    * Increments version in package.json/package-lock.json
    * Rebuilds so that builds include new version
    * Generates a changelog from git history
    * Creates a commit
    * Tags the commit with the new version (#75)
    * Pushes to origin
    * Published to npm

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# To trigger a release, use npm version patch
+
+# Generate the change log from the git log
+echo "" >> CHANGELOG.md
+echo "### v$npm_package_version" >> CHANGELOG.md
+echo '' >> CHANGELOG.md
+git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"- %s" >> CHANGELOG.md
+
+# Build everything so they contain new version
+npm run build
+
+# Allow reviewing staged changes
+echo ""
+read -p "Ready for release? (Please review changelog) " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
+
+git add -A
