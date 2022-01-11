9f318f7007130a380b7dbc177da98dcf50ba7779
Peter West <peter.g.west@gmail.com>
2022-01-11 22:29:56 +0000

    Add test for extended name format

---
+++ test/extended-name-format.spec.ts
@@ -0,0 +1,13 @@
+import { bibtex, bibtexTidy, test } from './utils';
+
+const input = bibtex`
+@CUSTOMD{LDN3,
+  OPTIONS     = {labelalphanametemplatename=customd},
+  AUTHOR	    = {Vela, Luis and given={Ura Ru}, family={Juan}, suffix={Sr}, prefix={von}},
+}
+`;
+
+test('extended name format (BibLaTeX)', async () => {
+	// just check it parses
+	await bibtexTidy(input);
+});
