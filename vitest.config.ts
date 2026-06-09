8451d60f2532d4909953a813d4d3270c27c73998
Peter West <peter.g.west@gmail.com>
2026-06-09 22:03:18 +0100

    test: tooling for svelte component tests

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelteTesting } from "@testing-library/svelte/vite";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	plugins: [svelte(), svelteTesting()],
+	test: {
+		globals: true,
+		coverage: {
+			reporter: ["text", "lcov"],
+		},
+	},
+});
